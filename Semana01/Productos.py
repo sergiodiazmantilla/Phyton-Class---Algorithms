4975d80cd3687bfcd77a522456e6d53dd441fa8b
Wistermundo Sergio Diaz Mantilla <sergiodmantilla@outlook.com>
2026-04-12 23:15:54 -0500

    Subiendo Semana 1 Clase 1

---
+++ Semana01/Productos.py
@@ -0,0 +1,17 @@
+#INSTALACION DE EDITOR DE CODIGO
+#https://code.visualstudio.com/download
+#INSTALACION DE PYTHON (+ EXTENSION DE PYTHON EN VSCODE)
+#https://www.python.org/downloads/
+#CREACION DE UN PROYECTO (Semana01)
+#CREACION DE UN ARCHIVO PYTHON (Productos.py)
+#DECLARACION DE VARIABLES
+nombre_producto = "Laptop"
+precio = 1500.00
+cantidad = 10
+#IMPRIMIR LOS VALORES DE LAS VARIABLES
+print("Nombre del producto:", nombre_producto)
+print("Precio:", precio)
+print("Cantidad:", cantidad)
+#REALIZAR UNA OPERACION
+total = precio * cantidad
+print("Total:", total)
