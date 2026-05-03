74c37bed9eedef0783322aa80fe36d95db57e3d6
Wistermundo Sergio Diaz Mantilla <sergiodmantilla@outlook.com>
2026-05-03 16:04:35 -0500

    Ordenando Archivos de Semana 1

---
+++ Semana01/Archivo_Python.py
@@ -0,0 +1,11 @@
+#1. INSTALACION DE EDITOR DE CODIGO
+#2. https://code.visualstudio.com/download
+#3. INSTALACION DE PYTHON (+ EXTENSION DE PYTHON EN VSCODE)
+#4. https://www.python.org/downloads/
+#5. CREACION DE UN PROYECTO (Semana01)
+#6. CREACION DE UN ARCHIVO PYTHON (Archivo_Python.py)
+
+#Hola Mundo
+print("¡Hola Mundo!")
+
+#Para continuar ver archivo Semana01/Productos.py
--- Semana01/Productos.py
+++ Semana01/Productos.py
@@ -1,17 +1,13 @@
-#INSTALACION DE EDITOR DE CODIGO
-#https://code.visualstudio.com/download
-#INSTALACION DE PYTHON (+ EXTENSION DE PYTHON EN VSCODE)
-#https://www.python.org/downloads/
-#CREACION DE UN PROYECTO (Semana01)
-#CREACION DE UN ARCHIVO PYTHON (Productos.py)
-#DECLARACION DE VARIABLES
+#7. DECLARACION DE VARIABLES
 nombre_producto = "Laptop"
 precio = 1500.00
 cantidad = 10
-#IMPRIMIR LOS VALORES DE LAS VARIABLES
+
+#8. IMPRIMIR LOS VALORES DE LAS VARIABLES
 print("Nombre del producto:", nombre_producto)
 print("Precio:", precio)
 print("Cantidad:", cantidad)
-#REALIZAR UNA OPERACION
+
+#9. REALIZAR UNA OPERACION E IMPRIMIR RESULTADOS
 total = precio * cantidad
 print("Total:", total)
